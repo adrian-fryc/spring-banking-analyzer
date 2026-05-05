14969117917c5f9100238907977d6c31fa98f9f2
Adrian Fryc <adrian.fryc@gmail.com>
2026-05-05 14:03:58 +0200

    Initial: Spring Boot project setup without IDE clutter

---
+++ src/main/java/pl/mentor/banking/analyzer/BankingAnalyzerApplication.java
@@ -0,0 +1,11 @@
+package pl.mentor.banking.analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingAnalyzerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BankingAnalyzerApplication.class, args);
+    }
+}
